b8bc5af06cd4d8755e5ee2842005aac52b7a37fd
ryan <chenvista@gmail.com>
2017-03-06 16:59:46 +0800

    stack 进制转换

--- stack/src/stack_demo.cpp
+++ stack/src/stack_demo.cpp
@@ -11,35 +11,75 @@
 #include "Coordinate.h"
 using namespace std;
 
-int main() {
-	MyStack<Coordinate> *pStack = new MyStack<Coordinate>(5);
-
-	pStack->push(Coordinate(1,2));  //底
-	pStack->push(Coordinate(2,3));
-	pStack->push(Coordinate(3,4));
-	pStack->push(Coordinate(4,5));
-	pStack->push(Coordinate(5,6));	//顶
-
-	pStack->stackTraverse(true);
 
 
-	pStack->stackTraverse(false);
-//	pStack->clearStack();
-	cout << endl;
-	cout << pStack->stackLength()<<endl;
+//int main() {
+//	MyStack<Coordinate> *pStack = new MyStack<Coordinate>(5);
+//
+//	pStack->push(Coordinate(1,2));  //底
+//	pStack->push(Coordinate(2,3));
+//	pStack->push(Coordinate(3,4));
+//	pStack->push(Coordinate(4,5));
+//	pStack->push(Coordinate(5,6));	//顶
+//
+//	pStack->stackTraverse(true);
+//
+//
+//	pStack->stackTraverse(false);
+////	pStack->clearStack();
+//	cout << endl;
+//	cout << pStack->stackLength()<<endl;
+//
+//	if(pStack ->stackEmpty())
+//	{
+//		cout << "栈为空" <<endl;
+//	}
+//
+//	if(pStack->stackFull())
+//	{
+//		cout << "栈为满" << endl;
+//
+//	}
+//
+//	delete pStack;
+//	pStack = NULL;
+//	return 0;
+//}
+#define BINARY 2
+#define OCTONARy 8
+#define HEXADECIMAL 16
 
-	if(pStack ->stackEmpty())
+int main(void)
+{
+	char num[] = "0123456789ABCDEF";
+	MyStack<int> *pStack = new MyStack<int>(30);
+	int N = 2017;
+	int mod = 0;
+
+	while(N != 0)
 	{
-		cout << "栈为空" <<endl;
+		mod = N % HEXADECIMAL;
+		pStack->push(mod);
+		N = N / HEXADECIMAL;
 	}
 
-	if(pStack->stackFull())
+//	pStack->stackTraverse(false);
+//	for(int i = pStack->stackLength()-1;i>=0;i--)
+//	{
+//		num[pStack[i]]
+//	}
+	int elem = 0;
+	while(!pStack->stackEmpty())
 	{
-		cout << "栈为满" << endl;
-
+		pStack->pop(elem);
+		cout << num[elem];
 	}
 
 	delete pStack;
 	pStack = NULL;
 	return 0;
 }
+
+
+
+
