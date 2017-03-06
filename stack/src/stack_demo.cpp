67e714447bdf511d495e2c92768e6399253ce078
ryan <chenvista@gmail.com>
2017-03-06 17:59:48 +0800

    stack 括号匹配

--- stack/src/stack_demo.cpp
+++ stack/src/stack_demo.cpp
@@ -45,41 +45,106 @@ using namespace std;
 //	pStack = NULL;
 //	return 0;
 //}
-#define BINARY 2
-#define OCTONARy 8
-#define HEXADECIMAL 16
-
-int main(void)
-{
-	char num[] = "0123456789ABCDEF";
-	MyStack<int> *pStack = new MyStack<int>(30);
-	int N = 2017;
-	int mod = 0;
-
-	while(N != 0)
-	{
-		mod = N % HEXADECIMAL;
-		pStack->push(mod);
-		N = N / HEXADECIMAL;
-	}
-
-//	pStack->stackTraverse(false);
-//	for(int i = pStack->stackLength()-1;i>=0;i--)
+//
+//#define BINARY 2
+//#define OCTONARy 8
+//#define HEXADECIMAL 16
+//
+//int main(void)
+//{
+//	char num[] = "0123456789ABCDEF";
+//	MyStack<int> *pStack = new MyStack<int>(30);
+//	int N = 2017;
+//	int mod = 0;
+//
+//	while(N != 0)
 //	{
-//		num[pStack[i]]
+//		mod = N % HEXADECIMAL;
+//		pStack->push(mod);
+//		N = N / HEXADECIMAL;
 //	}
-	int elem = 0;
-	while(!pStack->stackEmpty())
+//
+////	pStack->stackTraverse(false);
+////	for(int i = pStack->stackLength()-1;i>=0;i--)
+////	{
+////		num[pStack[i]]
+////	}
+//	int elem = 0;
+//	while(!pStack->stackEmpty())
+//	{
+//		pStack->pop(elem);
+//		cout << num[elem];
+//	}
+//
+//	delete pStack;
+//	pStack = NULL;
+//	return 0;
+//}
+#include <string.h>
+
+int main(void){
+	MyStack<char> *pStack = new MyStack<char>(30);
+	MyStack<char> *pNeedStack = new MyStack<char>(30);
+
+
+	char str[]="[[()]])";
+	char currentNeed = 0;
+	for(int i = 0; i<strlen(str); i++)cout << "字符串括号不匹配" <<endl;
 	{
-		pStack->pop(elem);
-		cout << num[elem];
+		if(str[i] != currentNeed)
+		{
+			pStack->push(str[i]);
+			switch(str[i])
+			{
+			case '[':
+				if(currentNeed!=0)
+				{
+					pNeedStack->push(currentNeed);
+				}
+				currentNeed=']';
+				break;
+			case '(':
+				if(currentNeed!=0)
+				{
+					pNeedStack->push(currentNeed);
+				}
+				currentNeed=')';
+				break;
+			default:
+				cout << "字符串括号不匹配,默认" <<endl;
+				return 0;
+			}
+		}
+		else
+		{
+			char elem;
+			pStack->pop(elem);
+			if(!pNeedStack->pop(currentNeed))
+			{
+				currentNeed = 0;
+			}
+		}
+
 	}
+
+
+	if(pStack->stackEmpty())
+	{
+		cout << "字符串括号匹配" <<endl;
+	}
+	else
+	{
+		cout << "字符串括号不匹配" <<endl;
+	}
+
 
 	delete pStack;
 	pStack = NULL;
+
+	delete pNeedStack;
+	pNeedStack = NULL;
+
 	return 0;
 }
-
-
 
 
